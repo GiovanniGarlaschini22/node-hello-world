ef8941555cefc8614e5742c78e96ec59fd8840b9
GiovanniGarlaschini22 <giovannigarlaschini.boolean@gmail.com>
2025-05-21 17:09:00 +0200

    Feat: punto 1 bonus fatto utilizzando process.argv per restituire nuovi valori e stamparli nel terminale con "node Index.js Hello Boolean"

--- Index.js
+++ Index.js
@@ -1,1 +1,12 @@
+// IMPORT //
+import printNewString from "./functions/functions.js";
+
+
 console.log("Hello World");
+// stampo la funzione //
+console.log(printNewString());
+
+// utilizzo variabile process con chiave argv //
+const hello = process.argv[2];
+const name = process.argv[3];
+console.log(printNewString(hello, name));
---
+++ functions/functions.js
@@ -0,0 +1,9 @@
+// funzione per stampare nuova stringa //
+function printNewString(helloValue, nameValue) {
+    return {
+        hello: helloValue,
+        name: nameValue,
+    };
+};
+// esporto funzione //
+export default printNewString;
